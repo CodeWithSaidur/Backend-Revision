e7538c7420ca4a24e5452b045c7fb36ae37f34c2
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-04-19 22:01:33 +0530

    3 out of 25

--- 3 out of 25 - How to connect frontend and backend in javascript  Fullstack Proxy and CORS/frontend/src/App.jsx
+++ 3 out of 25 - How to connect frontend and backend in javascript  Fullstack Proxy and CORS/frontend/src/App.jsx
@@ -3,15 +3,14 @@ import './App.css';
 
 function App() {
   const [products, setProducts] = useState([]);
-
   return (
     <>
       <h1>Code</h1>
       <p>{products.length}</p>
       {products.map((product) => {
-        <h3>{product.id}</h3>,
-        <p>{product.name}</p>,
-        <p>{product.description}</p>
+        (<h3>{product.id}</h3>),
+          (<p>{product.name}</p>),
+          (<p>{product.description}</p>);
       })}
     </>
   );
