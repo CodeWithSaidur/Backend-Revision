563f0852f6b0a9ea8c8af1e1913475d8c1575156
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-04-20 13:14:20 +0530

    3 out of 25

--- 3 out of 25 - How to connect frontend and backend in javascript  Fullstack Proxy and CORS/frontend/src/App.jsx
+++ 3 out of 25 - How to connect frontend and backend in javascript  Fullstack Proxy and CORS/frontend/src/App.jsx
@@ -1,19 +1,47 @@
-import { useState } from 'react';
-import './App.css';
+// App.js
 
-function App() {
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/products')
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },[]);
+
   return (
-    <>
-      <h1>Code</h1>
-      <p>{products.length}</p>
-      {products.map((product) => {
-        (<h3>{product.id}</h3>),
-          (<p>{product.name}</p>),
-          (<p>{product.description}</p>);
-      })}
-    </>
+    <div>
+      <h1>Top {products.length} Products</h1>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          style={{
+            border: '2px solid white',
+            borderRadius: '10px',
+          }}
+        >
+          <h4>{product.id}</h4>
+          <p>{product.name}</p>
+          <p>{product.category}</p>
+          <p>{product.price}</p>
+          <p>{product.description}</p>
+          <p>{product.brand}</p>
+          <p>{product.colors}</p>
+          <p>{product.stock}</p>
+          <p>{product.rating}</p>
+          <p>{product.reviews}</p>
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
