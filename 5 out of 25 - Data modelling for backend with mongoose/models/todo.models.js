730fb23340557de561b03e921d9a5dd51c001db3
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-04-26 23:36:55 +0530

    6 out of 25

---
+++ 5 out of 25 - Data modelling for backend with mongoose/models/todo.models.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+// Todo.model.js
+const TodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    createdBy: {
+      // making a relation between todo and user
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+
+    subtodo: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodo',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Todo = mongoose.model('Todo', TodoSchema);
