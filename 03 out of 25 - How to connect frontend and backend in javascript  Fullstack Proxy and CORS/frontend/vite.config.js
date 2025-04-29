7ab62e845d58a65f96238062e501aa3c50515a71
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-04-29 08:18:06 +0530

    03 out of 25

---
+++ 03 out of 25 - How to connect frontend and backend in javascript  Fullstack Proxy and CORS/frontend/vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  plugins: [react()],
+});
