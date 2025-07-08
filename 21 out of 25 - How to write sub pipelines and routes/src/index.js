d1c44109cb741c403bee08b4ba7252cf76ec01b8
saidur Rohman Barbhuiya <132422574+sabed2023@users.noreply.github.com>
2025-07-08 19:04:14 +0530

    21 out of 25

---
+++ 21 out of 25 - How to write sub pipelines and routes/src/index.js
@@ -0,0 +1,17 @@
+// index.js
+import { app } from './app.js';
+import dotenv from 'dotenv';
+import connectDb from './db/index.js';
+
+dotenv.config();
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 3030, () => {
+      console.log(`click http://localhost:${process.env.PORT}`);
+    });
+  })
+
+  .catch((err) => {
+    console.log('Db Connection failed');
+  });
